e4de266bfeb7c9a28385629766499c9fc30770f1
bhathiya fernando <26029390+bathiya-fernando@users.noreply.github.com>
2025-11-14 16:39:09 +0000

    fixed footer css class name issue

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
-    <footer class="bg-white bg-gray-800">
-      <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-        <span class="text-sm text-gray-500 sm:text-center text-gray-400">
+    <footer className="bg-white bg-gray-800">
+      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <span className="text-sm text-gray-500 sm:text-center text-gray-400">
           <span>Exam Scheduler</span> v0.0.1
         </span>
       </div>
